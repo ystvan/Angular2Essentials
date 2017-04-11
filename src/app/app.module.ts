4139d5f657b89f21747afe7fd44bade58525a909
Istvan Marki <istv0050@edu.easj.dk>
2017-04-11 13:08:53 +0200

    commenting app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,26 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+//Decorator from the core package, prefixed with @
+// imports, declarations, bootstrap => metadata properties
 @NgModule({
+
+  //is used to make components, directives and pipes available
   declarations: [
     AppComponent
   ],
+  //is used to bring in other angular modules
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
   providers: [],
+
+  //used for a root module, let angular know which component(s) will be the starting point for the process
+  //the entry point for my app code
   bootstrap: [AppComponent]
 })
+
+//app root module
 export class AppModule { }
