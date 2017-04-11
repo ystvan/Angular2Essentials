dd40ea04f2ee2b61e23bd34f417bd85808c00079
Istvan Marki <istv0050@edu.easj.dk>
2017-04-11 19:53:46 +0200

    built in validators

--- src/app/media-item-form/media-item-form.component.ts
+++ src/app/media-item-form/media-item-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-media-item-form',
@@ -12,7 +12,10 @@ export class MediaItemFormComponent {
   ngOnInit(){
     this.form = new FormGroup({
       medium: new FormControl('Movies'),
-      name: new FormControl(''),
+      name: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('[\\w\\-\\s\\/]+')
+      ])),
       category: new FormControl(''),
       year: new FormControl(''),
     });
